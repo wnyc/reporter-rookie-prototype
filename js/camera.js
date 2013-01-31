805395e3c2cb01a450120fff0f8de25e92f010a5
Scott Seaward <scottseaward@gmail.com>
2013-01-31 14:39:53 -0500

    Change assignment to action

--- js/camera.js
+++ js/camera.js
@@ -9,17 +9,24 @@
     var $video = $camera.find('video');
     $video.attr('autoplay', true);
     var stream;
+    var attemptingCapture = false;
 
     $section.on('activated', function() {
+      if (attemptingCapture) return;
+      attemptingCapture = true;
       // activate the camera
       navigator.webkitGetUserMedia({video: true}, function(localMediaStream) {
+        attemptingCapture = false;
         $video.attr('src', window.URL.createObjectURL(stream = localMediaStream));
-        if ($section.get(0) !== $('.active').get(0)) stream.stop();
+        if ($section.get(0) !== $('.active').get(0)) {
+          stream.stop();
+        }
       }, err);
     });
     $section.on('deactivated', function() {
       // deactivate the camera
       if (stream) {
+        attemptingCapture = false;
         stream.stop();
       }
     });
