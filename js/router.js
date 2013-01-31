c51e80b20c1e284d73b00426c27019bcf6e10b2a
Scott Seaward <scottseaward@gmail.com>
2013-01-31 11:39:17 -0500

    First commit

---
+++ js/router.js
@@ -0,0 +1,23 @@
+(function() {
+  var Router = Backbone.Router.extend({
+    routes: {
+      'screen/:screen_id': 'screen'
+    , '*path': 'home'
+    },
+    show: function(selector) {
+      $('section.active').removeClass('active');
+      $('section' + selector).addClass('active');
+    },
+    screen: function(screenID) {
+      this.show('#' + screenID);
+    },
+    goTo: function(place) {
+      this.navigate('screen/'+place, true);
+    },
+    home: function() {
+      this.goTo('login');
+    }
+  });
+
+  window.router = new Router;
+})();
